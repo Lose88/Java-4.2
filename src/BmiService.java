23569c220d9c4b2b8f6f481c31a13a845304c6d0
Lose88 <hotsting1888@gmail.com>
2022-06-20 21:34:26 +0300

    add pow in service

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,8 +1,22 @@
 public class BmiService {
-    public double calculate(double w, double h) {
-        double bmiIndex;
-        bmiIndex = w / (h * 2);
-        return bmiIndex;
-
+    public double pow(double height) {
+        return height * height;
     }
+    public double calculate(double weight, double height) {
+        double bmiIndex;
+        bmiIndex = weight / height;
+        return bmiIndex;
+    }
+
 }
+
+
+//public double calculate(double weight, double height) {
+
+//  double bmiIndex;
+
+//  bmiIndex = weight / height;
+//  return bmiIndex;
+
+// }
+//}
--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,12 @@ public class Main {
     public static void main(String[] args) {
 
         BmiService service = new BmiService();
-        double weight = 98.0;
+        double weight = 100.0;
         double height = 1.86;
+        service.pow(height);
 
-        double index = service.calculate(weight, height);
+
+        double index = service.calculate(weight, service.pow(height));
         System.out.println("Ваш индекс массы тела: " + index);
 
     }
